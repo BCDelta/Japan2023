b10d061eae1759e588e64c41a3e29684d8b3ffbc
Bryan <chan.bryan13@gmail.com>
2023-01-22 13:23:07 -0800

    Restored webpage with content

---
+++ public/assets/js/cityDescriptions.js
@@ -0,0 +1,25 @@
+   
+const tokyoInfo = `Tokyo, Japan’s busy capital, mixes the ultramodern and the traditional, from neon-lit skyscrapers to historic 
+                    temples. The opulent Meiji Shinto Shrine is known for its towering gate and surrounding woods. The 
+                    Imperial Palace sits amid large public gardens. The city's many museums offer exhibits ranging from classical art (in the Tokyo National Museum) 
+                    to a reconstructed kabuki theater (in the Edo-Tokyo Museum).`
+
+const osakaInfo = `Osaka is a large port city and commercial center on the Japanese island of Honshu. It's known for its modern architecture, nightlife and hearty street 
+                    food. The 16th-century shogunate Osaka Castle, which has undergone several restorations, is its main historical landmark. It's surrounded by a moat and park with plum, 
+                    peach and cherry-blossom trees. Sumiyoshi-taisha is among Japan’s oldest Shinto shrines.`
+
+const hokkaidoInfo = `Hokkaido, the northernmost of Japan’s main islands, is known for its volcanoes, natural hot springs (onsen) and ski areas. Rugged Daisetsuzan National 
+                        Park is home to steaming, volcanic Mount Asahi. Shikotsu-Tōya National Park contains caldera lakes, geothermal springs and a Mount Fuji look-alike, Mount Yōtei. 
+                        Popular ski resorts include Rusutsu, Furano and Niseko.`
+
+const kyotoInfo = `Kyoto, once the capital of Japan, is a city on the island of Honshu. It's famous for its numerous classical Buddhist temples, as well as 
+                    gardens, imperial palaces, Shinto shrines and traditional wooden houses. It’s also known for formal traditions such as kaiseki dining, consisting of 
+                    multiple courses of precise dishes, and geisha, female entertainers often found in the Gion district.`
+
+const saitamaInfo = `Saitama Prefecture is part of the Greater Tokyo area. In Saitama City, the Railway Museum traces rail history from the steam engines to 
+                        bullet trains. Kawagoe city is known for well-preserved Edo-era buildings. West, the 3 Kuroyama Santaki falls cascade down mountains. Chichibu city is home to 
+                        the 34 Buddhist temples of the Kannon Pilgrimage. Near Kinsho Temple, the Iwadatami rock formations line the Arakawa River.`
+
+const yokohamaInfo = `Yokohama, a Japanese city south of Tokyo, was one of the first Japanese ports opened to foreign trade, in 1859. 
+                        It contains a large Chinatown with hundreds of Chinese restaurants and shops. It’s also known for Sankei-en Garden, a botanical park containing preserved 
+                        Japanese residences from different eras, and the seaside Minato Mirai district, site of the 296m Landmark Tower.`
---
+++ src/components/Tokyo.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Tokyo(props) {
+    return (
+        <div>
+            <h2>{props.title}</h2>
+            <h2>{props.description}</h2>
+        </div>
+    )
+}
